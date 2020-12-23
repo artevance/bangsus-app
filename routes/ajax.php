ff9072cc9cc7fcaba33383d89c902df2f1fa64a5
artevance <agustinus.michael29@gmail.com>
2020-12-23 15:07:01 +0700

    Make new ajax routing

---
+++ routes/ajax.php
@@ -0,0 +1,14 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Ajax Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register Ajax routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your Ajax!
+|
+*/
